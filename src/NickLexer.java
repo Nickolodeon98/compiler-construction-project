7846e4665ffe229131e4558d244bb4ee39ab9e97
Seunghwan Jeon <fy16sj@leeds.ac.uk>
2019-03-05 23:04:28 +0000

    NextToken method on work

--- src/NickLexer.java
+++ src/NickLexer.java
@@ -1,38 +1,102 @@
+import java.io.BufferedReader;
 import java.io.FileReader;
-import java.IOException;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
-public enum TokenTypes {Symbol, Reservedw, Constant, Identifier}
+class NickLexer {
+  private BufferedReader current;
+  private char[] array;
+  private int lineNumber;
 
-public class Token {
-  public String Lexeme;
-  public TokenTypes Type;
-
-  public Token() {
-    this(null, null);
+  public static enum TokenTypes {
+    Symbol, Reservedw, Constant, Identifier;
   }
-}
+  public static class Token {
+    public String Lexeme;
+    public TokenTypes Type;
+    public int line;
+    public int column;
 
-class Lexer {
-  private FileReader current;
-  private int line;
+    public Token() {
+      Lexeme = null;
+      Type = null;
+    }
+  }
 
-  public bool init(String filename) {
+  public boolean init(String filename) {
     File newFile = new File(filename);
     if (newFile.exists() == false) {
       System.out.println("File " + filename + " does not exist.");
       return false;
     }
     try {
-      current = new FileReader(filename);
+      current = new BufferedReader(new FileReader(filename));
     }
     catch (FileNotFoundException error) {
       System.out.println("Unable to open file " + filename);
       return false;
     }
-    line = 1;
+    lineNumber = 1;
     return true;
   }
-  public int read() throws IOException
-  //e.g. current.read()
+
+  public void readFile(int offset, int length) {
+    try {
+      current.read(array, offset, length);
+    } catch (IOException error) {
+      System.out.println("Failed to read!");
+    }
+//    lineNumber++;
+  }
+
+  public void closeFile() {
+    try {
+      current.close();
+    }
+    catch (IOException error) {
+      System.out.println(error.getMessage());
+    }
+  }
+
+  public Token NextToken()
+  {
+    char start;
+    Token word = new Token();
+    long skipped = 0;
+
+    try { for( char i : array ) {
+      while (Character.isWhitespace(i)) {
+        skipped = skipped + current.skip(1);
+      }
+      start = i;
+      if (start instanceof char) {
+
+        word.Type = Reservedw;
+        System.out.println("This is Reservedw");
+      }
+    }
+    } catch (IOException error) {
+      System.out.println(error.getMessage());
+    }
+    return word;
+  }
+
+  public static void main(String[] args) {
+//    char[] myArray;
+//    myArray = new char[100];
+    int i;
+
+    System.out.println("Hello Java");
+    NickLexer myLexer = new NickLexer();
+    boolean judge = myLexer.init("samplecode.cs");
+    System.out.println(judge);
+    myLexer.readFile(0, 100);
+    // myLexer.closeFile();
+    Token token = myLexer.NextToken();
+    System.out.println(token.Type);
+//    for(i = 0; i < 100; i++) {
+//      System.out.println(myArray[i]);
+//    }
+  }
 }
